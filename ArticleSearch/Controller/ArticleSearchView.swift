11f5b3bfdefc6aefb1c49d55d4c63a302ab8bc51
Naga Ram Reddy Kancharla <nagaramreddykancharla@Nagas-MacBook-Pro.local>
2019-04-14 12:37:11 -0400

    Initial Commit

--- ArticleSearch/Controller/ArticleSearchView.swift
+++ ArticleSearch/Controller/ArticleSearchView.swift
@@ -33,7 +33,18 @@ class ArticleSearchView: UIViewController, UITableViewDelegate, UITableViewDataS
             let headlineDict = resultDict["headline"] as! Dictionary<String,AnyObject>
             let headlineString = headlineDict["print_headline"]
             cell.titleLabel?.text = headlineString as? String
-            cell.datePublished?.text = resultDict["pub_date"] as? String
+            
+            let date = (resultDict["pub_date"] as? String)!
+            let dateFormatterGet = DateFormatter()
+            dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            
+            let dateFormatterPrint = DateFormatter()
+            dateFormatterPrint.dateFormat = "MMM dd,yyyy"
+           
+            // again convert your date to string
+            let formattedDate = dateFormatterGet.date(from:date)!
+            let myStringafd = dateFormatterPrint.string(from: formattedDate)
+            cell.datePublished?.text = myStringafd
             return cell
         }else{
             return cell
